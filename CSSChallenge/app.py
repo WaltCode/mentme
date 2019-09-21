0c2d81fd3369196a2a9611946840df1d6cace0fb
Hameed Olawale <walham203378@gmail.com>
2019-09-21 10:33:59 -0700

    mentme

---
+++ CSSChallenge/app.py
@@ -0,0 +1,44 @@
+# k = [1,2,3,4,5,6,7,8,9,10]
+# print (k[-1:6:-1])
+
+
+# for x in [1, 2, 3, 4]:
+#     print (x)
+
+
+# for x in range(10):
+#     print (x, x*x, x*x*x)
+
+# for x in [1, 2, 3, 4]:
+#     print (x, x*x, x*x*x)
+
+# 
+
+# sum(["hello", "world"]) to be visited;
+
+
+def factorial():
+
+# x = [0, 1, [2]]
+# # x[2][0] = 3
+# # print(x) 
+# # x[2].append(4)
+# # print (x)
+# x[2] = 2
+# print (x)
+
+# l = ['a','s','d','f','g','a','d','f','a','d','a','r','e','e','w','a','d','a','d','a']
+# print(l.count('a'))
+# num = input('Enter a description:\n'))
+# if  num == "hell":
+#     print(num.replace('hell','****'))
+# elif  num == "fucking":
+#     print(num.replace('fucking','*******'))
+# elif  num == "fuck":
+#     print(num.replace('fuck','****'))
+# elif  num == "pussy":
+#     print(num.replace('pussy','******'))
+# elif  num == "damn":
+#     print(num.replace('damn','****'))
+# else:
+#     print(num)
